f5413754bb89ef9f627f07805d0e429a4d58c9d0
Addison Dalton <zarch14@gmail.com>
2023-11-12 23:02:39 -0500

    ballastic projectiles collisions detection

--- scenes/projectiles/ballastic_projectile.gd
+++ scenes/projectiles/ballastic_projectile.gd
@@ -11,3 +11,13 @@ func initialize(pos: Vector2, _rotation: float, initial_velocity: Vector2):
 	# with the rotated call
 	initial_velocity.y = -(abs(initial_velocity.y) + speed)
 	linear_velocity = initial_velocity.rotated(_rotation)
+
+
+
+func _on_body_entered(body):
+	set_deferred("freeze", true)
+	$AnimationPlayer.play("collision_explosion")
+
+# destroy the projectile when timer timesout
+func _on_timer_timeout():
+	queue_free()
